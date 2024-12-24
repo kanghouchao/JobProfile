c40918528d5bd3ae8275c92367846d0125726d15
カン <hchkang8710@gmail.com>
2024-12-24 21:50:40 +0900

    endpointを訂正する

--- backend/src/main/java/com/kang/job/auth/controller/RegisterController.java
+++ backend/src/main/java/com/kang/job/auth/controller/RegisterController.java
@@ -1,17 +1,19 @@
 package com.kang.job.auth.controller;
 
 import com.kang.job.auth.entity.User;
-import com.kang.job.auth.model.EmailSenderRequest;
 import com.kang.job.auth.model.PasswordSettingRequest;
 import com.kang.job.auth.service.RegisterService;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,9 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @Log4j2
+@Validated
 @RestController
+@RequestMapping("register")
 @AllArgsConstructor
-@RequestMapping
 public class RegisterController {
 
     private final RegisterService registerService;
@@ -29,15 +32,21 @@ public class RegisterController {
     /**
      * Send Email for new user
      *
-     * @param emailSenderRequest user's email
+     * @param email user's email
      * @return response
      */
-    @PostMapping("register")
-    public ResponseEntity<?> register(@RequestBody @Valid EmailSenderRequest emailSenderRequest) throws MessagingException {
-        this.registerService.sendEmail(emailSenderRequest.email());
+    @PostMapping("send-email")
+    public ResponseEntity<?> sendEmail(@RequestParam @Email final String email) throws MessagingException {
+        this.registerService.sendEmail(email);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * create user by token and password
+     *
+     * @param request token and password
+     * @return userId
+     */
     @PostMapping("create-user")
     public ResponseEntity<Long> createUser(@Valid @RequestBody PasswordSettingRequest request) {
         final User user = this.registerService.createUser(request.token(), request.password());
