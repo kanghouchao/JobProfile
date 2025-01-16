a89c60f7035d9f49b07e470dea91283786098d68
カン <hchkang8710@gmail.com>
2025-01-16 23:11:28 +0900

    データのチェック

--- backend/src/main/java/com/kang/jobprofile/user/auth/domain/RegistrationToken.java
+++ backend/src/main/java/com/kang/jobprofile/user/auth/domain/RegistrationToken.java
@@ -1,11 +1,13 @@
 package com.kang.jobprofile.user.auth.domain;
 
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
 /**
  * @author kanghouchao
  */
-public record RegistrationToken(String email, String token, LocalDateTime expiryTime) {
+public record RegistrationToken(String email, @Id String token, LocalDateTime expiryTime) {
 
     public boolean isExpired() {
         return expiryTime.isBefore(LocalDateTime.now());
--- backend/src/main/java/com/kang/jobprofile/user/auth/infrastructure/TokenRepository.java
+++ backend/src/main/java/com/kang/jobprofile/user/auth/infrastructure/TokenRepository.java
@@ -1,7 +1,10 @@
 package com.kang.jobprofile.user.auth.infrastructure;
 
 import com.kang.jobprofile.user.auth.domain.RegistrationToken;
+import org.hibernate.annotations.processing.SQL;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /**
  * @author kanghouchao
@@ -9,7 +12,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TokenRepository {
 
-    RegistrationToken findByToken(String token);
+    @SQL("SELECT rt.* FROM registration_token rt WHERE rt.email = :email and rt.expiry_time > :now limit 1")
+    RegistrationToken findByEmail(String email, LocalDateTime now);
 
     void save(RegistrationToken registrationToken);
 
