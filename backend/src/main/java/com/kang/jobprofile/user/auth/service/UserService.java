c72ba0a25ccfa689ee4eae12d37deb0249470e10
カン <hchkang8710@gmail.com>
2025-02-26 20:12:53 +0900

    add tables for resume and user

---
+++ backend/src/main/java/com/kang/jobprofile/user/auth/service/UserService.java
@@ -0,0 +1,24 @@
+package com.kang.jobprofile.user.auth.service;
+
+import com.kang.jobprofile.user.auth.domain.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author kanghouchao
+ */
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.getUserByEmail(username);
+    }
+
+}
