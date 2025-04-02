5a9f913f78eeb5056391007f5a212f8f9bb6e637
カン <hchkang8710@gmail.com>
2025-04-02 22:48:28 +0900

    rebuild

--- backend/src/main/java/com/kang/jobprofile/mail/model/Mail.java
+++
@@ -1,8 +0,0 @@
-package com.kang.jobprofile.mail.model;
-
-/**
- * @author kanghouchao
- */
-public record Mail(String from, String to, String subject, String content) {
-
-}
--- frontend/src/services/auth/authService.js
+++
@@ -1,9 +0,0 @@
-import httpClient from '../../config/HttpClinet';
-
-const userService = {
-  login: (loginData) => httpClient.postForm('login', loginData),
-  register: (email) => httpClient.putForm('auth/register/send-mail', {'email': email}),
-  createUser: (userData) => httpClient.post('auth/register/create-user', userData),
-};
-
-export default userService;
