67f1808869328fbee9a703b4f89121f4bea37da1
カン <hchkang8710@gmail.com>
2025-05-09 10:04:28 +0900

    i18n settings

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,25 +1,24 @@
-const { t } = require("i18next");
-const path = require("path");
+const path = require('path');
 
 module.exports = {
     webpack: {
         alias: {
-            "@": path.resolve(__dirname, "src"),
+            '@': path.resolve(__dirname, 'src'),
         },
     },
     eslint: {
         enable: true,
-        mode: "extends",
+        mode: 'extends',
         configure: {
-            extends: ["react-app", "react-app/jest"],
+            extends: ['react-app', 'react-app/jest'],
         },
     },
     babel: {
         presets: [
-            ["@babel/preset-env", { targets: { node: "current" } }],
-            ["@babel/preset-react", { runtime: "automatic" }],
+            ['@babel/preset-env', { targets: { node: 'current' } }],
+            ['@babel/preset-react', { runtime: 'automatic' }],
         ],
-        plugins: ["@babel/plugin-transform-runtime"],
+        plugins: ['@babel/plugin-transform-runtime'],
     },
     style: {
         postcss: {
@@ -32,12 +31,12 @@ module.exports = {
     jest: {
         configure: {
             moduleNameMapper: {
-                "^@/(.*)$": "<rootDir>/src/$1",
+                '^@/(.*)$': '<rootDir>/src/$1',
             },
             transform: {
-                "^.+\\.(js|jsx)$": "babel-jest",
+                '^.+\\.(js|jsx)$': 'babel-jest',
             },
-            testEnvironment: "jsdom",
+            testEnvironment: 'jsdom',
         },
     },
 };
--- frontend/src/config/i18n.js
+++ frontend/src/config/i18n.js
@@ -3,41 +3,40 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
-  en: {
-    components: require('@/locales/en/Components.json'),
-    pages: require('@/locales/en/Pages.json'),
-    auth: require('@/locales/en/Auth.json'),
-  },
-  zh: {
-    components: require('@/locales/zh/Components.json'),
-    pages: require('@/locales/zh/Pages.json'),
-    auth: require('@/locales/zh/Auth.json'),
-  },
-  ja: {
-    components: require('@/locales/ja/Components.json'),
-    pages: require('@/locales/ja/Pages.json'),
-    auth: require('@/locales/ja/Auth.json'),
-  },
+    en: {
+        components: require('@/locales/en/Components.json'),
+        pages: require('@/locales/en/Pages.json'),
+        auth: require('@/locales/en/Auth.json'),
+    },
+    zh: {
+        components: require('@/locales/zh/Components.json'),
+        pages: require('@/locales/zh/Pages.json'),
+        auth: require('@/locales/zh/Auth.json'),
+    },
+    ja: {
+        components: require('@/locales/ja/Components.json'),
+        pages: require('@/locales/ja/Pages.json'),
+        auth: require('@/locales/ja/Auth.json'),
+    },
 };
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    backend: {
-      loadPath: '@/locales/{{lng}}/{{ns}}.json',
-    },
-    preload: ['ja'],
-    fallbackLng: 'ja',
-    debug: false,
-    interpolation: { escapeValue: false },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
-    },
-    ns: ['components', 'pages', 'auth'],
-    defaultNS: 'auth',
-  });
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        backend: {
+            loadPath: '@/locales/{{lng}}/{{ns}}.json',
+        },
+        preload: ['ja'],
+        fallbackLng: 'ja',
+        debug: false,
+        interpolation: { escapeValue: false },
+        detection: {
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage'],
+        },
+        ns: ['components', 'pages', 'auth'],
+        defaultNS: 'auth',
+    });
 
 export default i18n;
