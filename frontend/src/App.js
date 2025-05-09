457e039677c0b991473fad02d752d90a0663edc5
カン <hchkang8710@gmail.com>
2025-05-09 11:37:57 +0900

    renew

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
+import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-      <ToastContainer />
-    </>
-  );
+    return (
+        <>
+            <RouterProvider router={router} />
+            <ToastContainer />
+        </>
+    );
 };
 
 export default App;
